7872788e82097ec80f0a80b5fc4e5b05c07dd861
Resurrect88 <evirakernel@gmail.com>
2020-11-11 10:36:41 +0100

    Merge tag 'v4.4.242' into Evira_Oldcam
    This is the 4.4.242 stable release

--- drivers/mmc/core/sdio_cis.c
+++ drivers/mmc/core/sdio_cis.c
@@ -34,6 +34,9 @@ static int cistpl_vers_1(struct mmc_card *card, struct sdio_func *func,
 	if (size < 2)
 		return 0;
 
+	if (size < 2)
+		return 0;
+
 	/* Find all null-terminated (including zero length) strings in
 	   the TPLLV1_INFO field. Trailing garbage is ignored. */
 	buf += 2;
